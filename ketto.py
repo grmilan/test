a2f7f10eadce5dc39498ee073a0beb8d8aeb6515
Gruber Milán <gruber.milan.lorant.2023f@ms.gdszeged.hu>
2024-10-01 08:38:16 +0200

    elso

---
+++ ketto.py
@@ -0,0 +1,29 @@
+print(" Cím: szövegek")
+
+szám = 2 + 4517
+print(szám)
+
+# x = int(input("Addj meg egy számot 1-10 között: "))
+# szám = x + 4517
+# print(szám)
+
+d = '1' + '2'
+szám = 2 + 4517
+print(d)
+print(d*2)
+majom = 'orángután' + 'cerkóf' # konkatentáció - összefűzés
+# print(majom)
+
+majom = "pávián" + "maki"
+print(majom)
+
+edény = 'bögre'
+edény = 'sárga' + edény + 'görbe' + edény
+print(edény)
+
+a = int(input("addj egy számot 1-10 között"))
+b = int(input("addj egy számot 1-10 között"))
+print (F' {a} és {b} összege {a+b}')
+
+print(f' szorzat: a*b {a*b} ')
+print(f' hatvány: a**b {a**b} ')
